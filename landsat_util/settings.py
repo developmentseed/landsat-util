c01d675c90551902e6d9e43b26a72b02ac583107
Scisco <alireza@developmentseed.org>
2014-08-01 17:21:10 -0400

    Updated settings file

--- landsat_util/settings.py
+++ landsat_util/settings.py
@@ -12,9 +12,20 @@
 
 import os
 
+# Google Storage Landsat Config
+
 SOURCE_URL = 'gs://earthengine-public/landsat'
 SCENE_FILE_URL = SOURCE_URL + '/scene_list.zip'
 SATELLITE = 'L8'
+L8_METADATA_URL = 'http://landsat.usgs.gov/metadata_service/bulk_metadata_files/LANDSAT_8.csv'
+
+# Elastic Search Config
+
+ES_URL = 'http://localhost:9200'
+ES_MAIN_INDEX = 'landsat'
+ES_MAIN_TYPE = '8'
+
+# Local Forlders Config
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 DOWNLOAD_DIR = BASE_DIR + '/output/imagery'
