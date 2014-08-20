df1c4de0b5843d37d5dce3255ad1564b27dfb050
Scisco <alireza@developmentseed.org>
2014-08-20 14:12:32 -0400

    Created tests and docstrings in search helper

--- landsat/search_helper.py
+++ landsat/search_helper.py
@@ -26,12 +26,49 @@ class Search(object):
                cloud_min=None,
                cloud_max=None,
                limit=1):
+        """
+        The main method of Search class. It searches the DevSeed Landsat API
 
-        search_string = self.query_builder(row_paths,
-                                           start_date,
-                                           end_date,
-                                           cloud_min,
-                                           cloud_max)
+        Returns python dictionary
+
+        Arguments:
+            row_paths -- A string in this format: "003,003,004,004". Must be in pairs
+            start_date -- date string. format: YYYY-MM-DD
+            end_date -- date string. format: YYYY-MM-DD
+            cloud_min -- float specifying the minimum percentage. e.g. 4.3
+            cloud_max -- float specifying the maximum percentage. e.g. 78.9
+            limit -- integer specigying the maximum results return.
+
+        Example:
+
+            search('003,003', '2014-01-01', '2014-06-01')
+
+            will return:
+
+            {
+                'status': u'SUCCESS',
+                'total_returned': 1,
+                'total': 1,
+                'limit': 1
+                'results': [
+                    {
+                        'sat_type': u'L8',
+                        'sceneID': u'LC80030032014142LGN00',
+                        'date': u'2014-05-22',
+                        'path': u'003',
+                        'thumbnail': u'http://earthexplorer.usgs.gov/browse/landsat_8/2014/003/003/LC80030032014142LGN00.jpg',
+                        'cloud': 33.36,
+                        'row': u'003
+                    }
+                ]
+            }
+        """
+
+        search_string = self._query_builder(row_paths,
+                                            start_date,
+                                            end_date,
+                                            cloud_min,
+                                            cloud_max)
 
         # Have to manually build the URI to bypass requests URI encoding
         # The api server doesn't accept encoded URIs
@@ -56,43 +93,52 @@ class Search(object):
                                   'sat_type': u'L8',
                                   'path': three_digit(i['path']),
                                   'row': three_digit(i['row']),
-                                  'thumbnail': i['browseURL']}
+                                  'thumbnail': i['browseURL'],
+                                  'date': i['acquisitionDate'],
+                                  'cloud': i['cloudCoverFull']}
                                  for i in r_dict['results']]
 
         return result
 
-    def query_builder(self,
-                      row_paths=None,
-                      start_date=None,
-                      end_date=None,
-                      cloud_min=None,
-                      cloud_max=None):
+    def _query_builder(self,
+                       row_paths=None,
+                       start_date=None,
+                       end_date=None,
+                       cloud_min=None,
+                       cloud_max=None):
+        """ Builds the proper search syntax (query) for Landsat API """
+
         query = []
         rows_paths = []
 
+        # Coverting rows and paths to paired list
         if row_paths:
             row_path_array = row_paths.split(',')
-            new_array = [tuple(row_path_array[i:i + 2])
-                         for i in range(0, len(row_path_array), 2)]
-            rows_paths.extend(['(%s)' % self.row_path_builder(i[0], i[1])
-                               for i in new_array])
+            ##Make sure the elements in the list are even and pairable
+            if len(row_path_array) % 2 == 0:
+                new_array = [tuple(row_path_array[i:i + 2])
+                             for i in range(0, len(row_path_array), 2)]
+                rows_paths.extend(['(%s)' % self._row_path_builder(i[0], i[1])
+                                   for i in new_array])
+            else:
+                return ''
 
         if start_date and end_date:
-            query.append(self.date_range_builder(start_date, end_date))
+            query.append(self._date_range_builder(start_date, end_date))
         elif start_date:
-            query.append(self.date_range_builder(start_date, '2100-01-01'))
+            query.append(self._date_range_builder(start_date, '2100-01-01'))
         elif end_date:
-            query.append(self.date_range_builder('2009-01-01', end_date))
+            query.append(self._date_range_builder('2009-01-01', end_date))
 
         if cloud_min and cloud_max:
-            query.append(self.cloud_cover_prct_range_builder(cloud_min,
-                                                             cloud_max))
+            query.append(self._cloud_cover_prct_range_builder(cloud_min,
+                                                              cloud_max))
         elif cloud_min:
-            query.append(self.cloud_cover_prct_range_builder(cloud_min,
-                                                             '100'))
+            query.append(self._cloud_cover_prct_range_builder(cloud_min,
+                                                              '100'))
         elif cloud_max:
-            query.append(self.cloud_cover_prct_range_builder('0',
-                                                             cloud_max))
+            query.append(self._cloud_cover_prct_range_builder('0',
+                                                              cloud_max))
 
         search_string = '+AND+'.join(map(str, query))
 
@@ -104,19 +150,19 @@ class Search(object):
 
         return search_string
 
-    def row_path_builder(self, row, path):
+    def _row_path_builder(self, row, path):
         """ Builds row and path query
             Accepts row and path in XXX format, e.g. 003
         """
         return 'row:%s+AND+path:%s' % (row, path)
 
-    def date_range_builder(self, start, end):
+    def _date_range_builder(self, start, end):
         """ Builds date range query
             Accepts start and end date in this format YYYY-MM-DD
         """
         return 'acquisitionDate:[%s+TO+%s]' % (start, end)
 
-    def cloud_cover_prct_range_builder(self, min, max):
+    def _cloud_cover_prct_range_builder(self, min, max):
         """ Builds cloud cover percentage range query
             Accepts bottom and top range in float, e.g. 1.00
         """
