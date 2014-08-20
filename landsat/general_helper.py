a9e9c2ed42d1b00f83f148e0dbcd029035d4e69a
Scisco <alireza@developmentseed.org>
2014-08-20 11:51:35 -0400

    Added tests and docstrings to general_helper

--- landsat/general_helper.py
+++ landsat/general_helper.py
@@ -25,6 +25,9 @@ class Capturing(list):
 
 
 def check_create_folder(folder_path):
+    """ Check whether a folder exists, if not the folder is created
+    Always return folder_path
+    """
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
         print "%s folder created" % folder_path
@@ -33,34 +36,58 @@ def check_create_folder(folder_path):
 
 
 def get_file(path):
+    """ Separate the name of the file or folder from the path and return it
+    Example: /path/to/file ---> file
+    """
     return os.path.basename(path)
 
 
 def get_filename(path):
+    """ Return the filename without extension. e.g. index.html --> index """
     return os.path.splitext(get_file(path))[0]
 
 
 def three_digit(number):
+    """ Add 0s to inputs that their length is less than 3.
+    For example: 1 --> 001 | 02 --> 020 | st --> 0st
+    """
     number = str(number)
     if len(number) == 1:
         return u'00%s' % number
     elif len(number) == 2:
         return u'0%s' % number
+    else:
+        return number
 
 
 def georgian_day(date):
-    fmt = '%m/%d/%Y'
-    return datetime.strptime(date, fmt).timetuple().tm_yday
+    """ Returns the number of days passed since the start of the year
+    Accepted format: %m/%d/%Y
+    """
+    try:
+        fmt = '%m/%d/%Y'
+        return datetime.strptime(date, fmt).timetuple().tm_yday
+    except (ValueError, TypeError):
+        return 0
 
 
 def year(date):
-    fmt = '%m/%d/%Y'
-    return datetime.strptime(date, fmt).timetuple().tm_year
+    """ Returns the year
+    Accepted format: %m/%d/%Y
+    """
+    try:
+        fmt = '%m/%d/%Y'
+        return datetime.strptime(date, fmt).timetuple().tm_year
+    except ValueError:
+        return 0
 
 
 def reformat_date(date, new_fmt):
-    if date:
+    """ Return reformated date. Example: 01/28/2014 & %d/%m/%Y -> 28/01/2014
+    Accepted date format: %m/%d/%Y
+    """
+    try:
         fmt = '%m/%d/%Y'
         return datetime.strptime(date, fmt).strftime(new_fmt)
-    else:
-        return None
+    except ValueError:
+        return date
