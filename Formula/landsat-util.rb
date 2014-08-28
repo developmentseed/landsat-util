41740bcf93e0d4f1e7079067fdb03d98030baafd
Scisco <jazayeri@gmail.com>
2014-08-28 18:51:01 -0400

    Added version 0.2

--- Formula/landsat-util.rb
+++ Formula/landsat-util.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class LandsatUtil < Formula
   homepage "http://www.developmentseed.org"
-  url "https://github.com/developmentseed/landsat-util/archive/v0.1.0.tar.gz"
+  url "https://github.com/developmentseed/landsat-util/archive/v0.2.0.tar.gz"
   sha1 "522e460c7cb8c229c3bf4e3d965b0c9f5924c1e9"
-  head "https://github.com/developmentseed/landsat-util.git", :branch => 'image'
+  head "https://github.com/developmentseed/landsat-util.git"
 
   depends_on "gdal"
   depends_on "libtiff"
