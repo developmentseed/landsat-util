ce5e1766d7dad40fbfe994f59e167aab244dd1d3
Jimmy Huang <jimmy@netivism.com.tw>
2015-02-13 23:19:54 +0800

    GDAL version correction.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(name="landsat",
       license="CCO",
       platforms="Posix; MacOS X; Windows",
       install_requires=[
-          "GDAL==1.11.0",
+          "GDAL>=1.11",
           "elasticsearch==1.1.1",
           "gsutil==4.4",
           "requests==2.3.0",
