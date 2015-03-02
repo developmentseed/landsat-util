894e5ce5037dea90e100bff7dffe7e7b99ccff08
Scisco <scisco7@gmail.com>
2015-03-02 13:38:45 -0500

    updated tests for landsat util

--- landsat/tests/test_landsat.py
+++ landsat/tests/test_landsat.py
@@ -7,16 +7,15 @@ import sys
 import errno
 import shutil
 import unittest
+import subprocess
 from tempfile import mkdtemp
 from os.path import join, abspath, dirname
 
 try:
     import landsat.landsat as landsat
-    import landsat.settings as settings
 except ImportError:
     sys.path.append(abspath(join(dirname(__file__), '../../landsat')))
     import landsat
-    import settings
 
 
 class TestLandsat(unittest.TestCase):
@@ -24,7 +23,6 @@ class TestLandsat(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.temp_folder = mkdtemp()
-        settings.BASE_DIR = cls.temp_folder
         cls.base_dir = abspath(dirname(__file__))
         cls.landsat_image = join(cls.base_dir, 'samples', 'test.tar.bz2')
         cls.parser = landsat.args_options()
@@ -33,6 +31,7 @@ class TestLandsat(unittest.TestCase):
     def tearDownClass(cls):
         try:
             shutil.rmtree(cls.temp_folder)
+            shutil.rmtree(join(cls.base_dir, 'samples', 'test'))
         except OSError as exc:
             if exc.errno != errno.ENOENT:
                 raise
@@ -41,7 +40,6 @@ class TestLandsat(unittest.TestCase):
         try:
             landsat.main(self.parser.parse_args(args))
         except SystemExit as e:
-            print e.message
             self.assertEqual(e.code, code)
 
     def test_incorrect_date(self):
@@ -80,45 +78,38 @@ class TestLandsat(unittest.TestCase):
 
     def test_download_correct(self):
         """Test download command with correct input"""
-        args = ['download', 'LT81360082013127LGN01']
-
-        print self.temp_folder
+        args = ['download', 'LC80010092015051LGN00', '-b', '11,', '-d', self.temp_folder]
 
         self.system_exit(args, 0)
 
-    # def test_download_incorrect(self):
-    #     """Test download command with incorrect input"""
-    #     args = ['download', 'LT813600']
+    def test_download_incorrect(self):
+        """Test download command with incorrect input"""
+        args = ['download', 'LT813600']
 
-    #     self.system_exit(args, 1)
+        self.system_exit(args, 1)
 
-    # def test_process_correct(self):
-    #     """Test process command with correct input"""
-    #     args = ['process', self.landsat_image]
+    def test_process_correct(self):
+        """Test process command with correct input"""
+        args = ['process', self.landsat_image]
 
-    #     self.system_exit(args, 0)
+        self.system_exit(args, 0)
 
-    # def test_process_correct_pansharpen(self):
-    #     """Test process command with correct input and pansharpening"""
-    #     args = ['process', '--pansharpen', self.landsat_image]
+    def test_process_correct_pansharpen(self):
+        """Test process command with correct input and pansharpening"""
+        args = ['process', '--pansharpen', self.landsat_image]
 
-    #     self.system_exit(args, 0)
+        self.system_exit(args, 0)
 
-    # def test_process_incorrect(self):
-    #     """Test process command with incorrect input"""
-    #     args = ['process', 'whatever']
+    def test_process_incorrect(self):
+        """Test process command with incorrect input"""
+        args = ['process', 'whatever']
 
-    #     self.system_exit(args, 1)
+        self.system_exit(args, 1)
 
-    # def check_package_installed(self):
-    #     """ Checks if the package is installed """
-    #     self.assertTrue(landsat.package_installed('python'))
-    #     self.assertFalse(landsat.package_installed('whateverpackage'))
+    def check_command_line(self):
+        """ Check if the commandline performs correctly """
 
-    # def check_command_line(self):
-    #     """ Check if the commandline performs correctly """
-
-    #     self.assertEqual(subprocess.call(['python', os.path.join(self.base_dir, '../landsat.py'), '-h']), 0)
+        self.assertEqual(subprocess.call(['python', join(self.base_dir, '../landsat.py'), '-h']), 0)
 
 
 if __name__ == '__main__':
