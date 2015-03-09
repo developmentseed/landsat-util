1da235daff76eb5e8629487e08c58bd2b5afd012
Scisco <scisco7@gmail.com>
2015-03-09 16:23:35 -0700

    scipy is a dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from landsat import __version__
 
 
 def readme():
-    with open("README.rst") as f:
+    with open('README.rst') as f:
         return f.read()
 
 test_requirements = [
@@ -20,28 +20,29 @@ test_requirements = [
 ]
 
 setup(
-    name="landsat",
+    name='landsat-util',
     version=__version__,
-    description="A utility to search, download and process Landsat 8" +
-    " satellite imagery",
+    description='A utility to search, download and process Landsat 8' +
+    ' satellite imagery',
     long_description=readme(),
-    author="Scisco",
-    author_email="alireza@developmentseed.org",
-    scripts=["bin/landsat"],
-    url="https://github.com/developmentseed/landsat-util",
-    packages=["landsat"],
+    author='Development Seed',
+    author_email='info@developmentseed.org',
+    scripts=['bin/landsat'],
+    url='https://github.com/developmentseed/landsat-util',
+    packages=['landsat'],
     include_package_data=True,
-    license="CCO",
-    platforms="Posix; MacOS X; Windows",
+    license='CCO',
+    platforms='Posix; MacOS X; Windows',
     install_requires=[
-      "requests==2.5.3",
-      "python-dateutil==2.2",
-      "numpy==1.9.1",
-      "termcolor==1.1.0",
-      "rasterio==0.18",
-      "six==1.9.0",
-      "scikit-image==0.10.1",
-      "homura==0.1.0"
+      'requests==2.5.3',
+      'python-dateutil==2.2',
+      'numpy==1.9.1',
+      'termcolor==1.1.0',
+      'rasterio==0.18',
+      'six==1.9.0',
+      'scipy==0.15.1',
+      'scikit-image==0.10.1',
+      'homura==0.1.0'
     ],
     test_suite='nose.collector',
     test_require=test_requirements
