49d11393449cf2c0b687921ae27934c71ce26ce0
Scisco <scisco7@gmail.com>
2015-05-29 15:22:32 +0100

    use mock objects to fix build issues on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -13,10 +13,18 @@
 # serve to show the default.
 
 import sys
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'rasterio', 'scipy', 'scikit-image', 'homura', 'boto']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 import os
-
 import sphinx_rtd_theme
-
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
