9095a1c1c7b15d239ed56dbe045d32b1e29fda95
Scisco <scisco7@gmail.com>
2015-05-29 15:25:32 +0100

    add all dependenceis to mock list

--- docs/conf.py
+++ docs/conf.py
@@ -15,12 +15,14 @@
 import sys
 from mock import Mock as MagicMock
 
+
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['numpy', 'rasterio', 'scipy', 'scikit-image', 'homura', 'boto']
+MOCK_MODULES = ['numpy', 'rasterio', 'scipy', 'scikit-image', 'homura', 'boto',
+                'termcolor', 'requests', 'python-dateutil']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 import os
