8b42d36a31de601fdc36973ffd96b027f460a765
Scisco <scisco7@gmail.com>
2015-06-29 18:11:14 -0400

    update ndvi with new base process class

--- landsat/ndvi.py
+++ landsat/ndvi.py
@@ -1,110 +1,51 @@
-import warnings
-from image import BaseProcess
 import rasterio
-from utils import timer, get_file, check_create_folder
-import sys
-import settings
 from os.path import join
 import numpy
 from skimage.util import img_as_ubyte
 
+from decorators import rasterio_decorator
+from image import BaseProcess
+
 
 class NDVI(BaseProcess):
-    """
-    NDVI Processing class
 
-    :param path:
-        Path of the image.
-    :type path:
-        String
-    :param verbose:
-        Whether the output should be verbose. Default is False. (optional)
-    :type verbose:
-        boolean
-    :param dst_path:
-        Path to the folder where the image should be stored. (optional)
-    :param force_unzip:
-        Whether to force unzip the tar file. Default is False
-    :type force_unzip:
-        boolean
-    """
-
-    def __init__(self, path, bands=[4, 5], dst_path=None, verbose=False, force_unzip=False):
+    def __init__(self, path, bands=None, dst_path=None, verbose=False, force_unzip=False):
+        bands = [4, 5]
         super(NDVI, self).__init__(path, bands, dst_path, verbose, force_unzip)
 
+    @rasterio_decorator
     def run(self):
         """
         Executes NDVI processing
         """
         self.output("* NDVI processing started.", normal=True)
 
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            with rasterio.drivers():
-                bands = []
-                try:
-                    for i, band in enumerate(self.bands):
-                        bands.append(self._read_band(self.bands_path[i]))
+        bands = self._read_bands()
+        image_data = self._get_image_data()
 
-                except IOError as e:
-                    exit(e.message, 1)
+        new_bands = []
+        for i in range(0, 2):
+            new_bands.append(numpy.empty(image_data['shape'], dtype=numpy.float32))
 
-                src = rasterio.open(self.bands_path[-1])
-                self.pixel = src.affine[0]
+        self._warp(image_data, bands, new_bands)
 
-                proj_data = {
-                    'transform': src.transform,
-                    'crs': src.crs,
-                    'affine': src.affine,
-                    'shape': src.shape
-                }
-                crn = self._get_boundaries(proj_data)
+        # Bands are no longer needed
+        del bands
 
-                dst_shape = proj_data['shape']
-                dst_corner_ys = [crn[k]['y'][1][0] for k in crn.keys()]
-                dst_corner_xs = [crn[k]['x'][1][0] for k in crn.keys()]
-                y_pixel = abs(max(dst_corner_ys) - min(dst_corner_ys)) / dst_shape[0]
-                x_pixel = abs(max(dst_corner_xs) - min(dst_corner_xs)) / dst_shape[1]
-                dst_transform = (min(dst_corner_xs),
-                                 x_pixel,
-                                 0.0,
-                                 max(dst_corner_ys),
-                                 0.0,
-                                 -y_pixel)
+        output_band = numpy.true_divide((new_bands[1] - new_bands[0]), (new_bands[1] + new_bands[0]))
 
-                new_bands = []
-                for i in range(0, 2):
-                    new_bands.append(numpy.empty(dst_shape, dtype=numpy.float32))
+        output_file = '%s_NDVI.TIF' % (self.scene)
+        output_file = join(self.dst_path, output_file)
 
-                self.output("Projecting", normal=True, arrow=True)
-                proj_data["dst_transform"] = dst_transform
-                self._warp(proj_data, bands, new_bands)
+        output = rasterio.open(output_file, 'w', driver='GTiff',
+                               width=image_data['shape'][1],
+                               height=image_data['shape'][0],
+                               count=1,
+                               dtype=numpy.uint8,
+                               nodata=0,
+                               transform=image_data['dst_transform'],
+                               crs=self.dst_crs)
 
-                for i in range(0, 2):
-                    new_bands[i] = new_bands[i].astype(numpy.float32)
-
-                output_band = numpy.true_divide((new_bands[1] - new_bands[0]), (new_bands[1] + new_bands[0]))
-
-                output_file = '%s_NDVI.TIF' % (self.scene)
-                output_file = join(self.dst_path, output_file)
-
-                output = rasterio.open(output_file, 'w', driver='GTiff',
-                                       width=dst_shape[1],
-                                       height=dst_shape[0],
-                                       count=1,
-                                       dtype=numpy.uint8,
-                                       nodata=0,
-                                       transform=dst_transform,
-                                       crs=self.dst_crs)
-
-                output.write_band(1, img_as_ubyte(output_band))
-                self.output("Writing to file", normal=True, color='green', indent=1)
-                return output_file
-
-
-if __name__ == '__main__':
-
-    with timer():
-        p = NDVI(sys.argv[1])
-
-        print p.run()
+        output.write_band(1, img_as_ubyte(output_band))
+        self.output("Writing to file", normal=True, color='green', indent=1)
+        return output_file
