792d7e5ede6f64b8d1d7c29a0007d1addf5f081d
Scisco <scisco7@gmail.com>
2015-09-30 12:06:50 +0200

    improve regex formula

--- landsat/utils.py
+++ landsat/utils.py
@@ -299,7 +299,9 @@ def convert_to_float_list(value):
     if isinstance(value, list) or value is None:
         return value
     else:
-        s = re.findall('([-+]?\d*\.\d+|\d+)', value)
+        print value
+        s = re.findall('([-+]?\d*\.\d+|\d+|[-+]?\d+)', value)
+        print s
         for k, v in enumerate(s):
             try:
                 s[k] = float(v)
