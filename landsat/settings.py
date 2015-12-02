15ac13787bab8e3b13b57a575c3ea46a11e1ec3c
scisco <scisco7@gmail.com>
2015-12-01 19:04:51 -0500

    keep slashes

--- landsat/settings.py
+++ landsat/settings.py
@@ -14,8 +14,8 @@ DEBUG = getenv('DEBUG', False)
 
 SATELLITE = 'L8'
 L8_METADATA_URL = 'http://landsat.usgs.gov/metadata_service/bulk_metadata_files/LANDSAT_8.csv'
-GOOGLE_STORAGE = 'http://storage.googleapis.com/earthengine-public/landsat'
-S3_LANDSAT = 'http://landsat-pds.s3.amazonaws.com'
+GOOGLE_STORAGE = 'http://storage.googleapis.com/earthengine-public/landsat/'
+S3_LANDSAT = 'http://landsat-pds.s3.amazonaws.com/'
 API_URL = 'https://api.developmentseed.org/landsat'
 
 # User's Home Directory
