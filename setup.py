10acbf4950c7afed16cdf18f85ddbdf757652a56
Scisco <scisco7@gmail.com>
2016-02-18 00:53:45 -0500

    take six to 1.8.0 because polyline requires it

--- setup.py
+++ setup.py
@@ -35,19 +35,22 @@ setup(
     license='CCO',
     platforms='Posix; MacOS X; Windows',
     install_requires=[
-        'git+git://github.com/developmentseed/usgs@develop',
+        'usgs==0.1.9',
         'requests==2.7.0',
         'python-dateutil>=2.4.2',
         'numpy>=1.9.3',
         'termcolor>=1.1.0',
         'rasterio>=0.26.0',
-        'six==1.9.0',
+        'six==1.8.0',
         'scipy>=0.16.0',
         'scikit-image>=0.11.3',
         'homura>=0.1.2',
         'boto>=2.38.0',
         'polyline==1.1',
         'geocoder>=1.5.1'
+    ],
+    dependency_links=[
+        "git+https://github.com/developmentseed/usgs@develop#egg=usgs-0.1.9"
     ],
     test_suite='nose.collector',
     tests_require=test_requirements
