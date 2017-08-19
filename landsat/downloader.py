5f3bd079652d89239d294d541787597d02d29b3f
Mellian <31151900+jmellian@users.noreply.github.com>
2017-08-19 21:52:39 +0200

    Download Collection 1 data from AWS
    Added download Collection 1 landsat 8 data from AWS after 1 may 2017:
    Example use: pre-collection en collection 1 Landsat 8 data
    - landsat download LC81970232017085LGN00 --bands 432
    - landsat download LC08_L1TP_139045_20170304_20170316_01_T1 --bands 432
    
    - landsat download LC81970232017085LGN00
    - landsat download LC08_L1TP_139045_20170304_20170316_01_T1

--- landsat/downloader.py
+++ landsat/downloader.py
@@ -68,7 +68,9 @@ class Downloader(VerbosityMixin):
                 # for all scenes if bands provided, first check AWS, if the bands exist
                 # download them, otherwise use Google and then USGS.
                 try:
-                    # if bands are not provided, directly go to Goodle and then USGS
+                    # if bands are not provided, directly go to Google and then USGS
+                    if not isinstance(bands, list):
+                       bands = [1,2,3,4,5,6,7,8,9,10,11]
                     if not isinstance(bands, list):
                         raise RemoteFileDoesntExist
                     files.append(self.amazon_s3(scene, bands))
@@ -90,6 +92,7 @@ class Downloader(VerbosityMixin):
         # download from usgs if login information is provided
         if self.usgs_user and self.usgs_pass:
             try:
+                print (self.usgs_user)
                 api_key = api.login(self.usgs_user, self.usgs_pass)
             except USGSError as e:
                 error_tree = ElementTree.fromstring(str(e.message))
@@ -125,6 +128,7 @@ class Downloader(VerbosityMixin):
 
         sat = self.scene_interpreter(scene)
         url = self.google_storage_url(sat)
+        print (url)
 
         self.remote_file_exists(url)
 
@@ -138,28 +142,52 @@ class Downloader(VerbosityMixin):
 
         sat = self.scene_interpreter(scene)
 
-        # Always grab MTL.txt and QA band if bands are specified
+        # Always grab QA band if bands are specified
+        urls = []
+
         if 'BQA' not in bands:
             bands.append('QA')
 
+
+        if len(scene) == 40:
+           for band in bands:
+               url2 = self.amazon_s3_url_type(sat, band, ".TIF.ovr")
+               #print (url2)
+
+               # make sure it exist
+               self.remote_file_exists(url2)
+               urls.append(url2)
+
+               url3 = self.amazon_s3_url_type(sat, band, "_wrk.IMD")
+               #print (url3)
+
+               # make sure it exist
+               self.remote_file_exists(url3)
+               urls.append(url3)
+
+        # Always grab MTL.txt and ANG band if bands are specified
         if 'MTL' not in bands:
             bands.append('MTL')
 
-        urls = []
+        if 'ANG' not in bands and len(scene) == 40:
+            bands.append('ANG')
 
         for band in bands:
             # get url for the band
             url = self.amazon_s3_url(sat, band)
+            #print (url)
 
             # make sure it exist
             self.remote_file_exists(url)
             urls.append(url)
+            
 
         # create folder
         path = check_create_folder(join(self.download_dir, scene))
 
         self.output('Source: AWS S3', normal=True, arrow=True)
         for url in urls:
+            #print(url)
             self.fetch(url, path)
 
         return path
@@ -192,7 +220,7 @@ class Downloader(VerbosityMixin):
 
         self.output('Downloading: %s' % filename, normal=True, arrow=True)
 
-        # print(join(path, filename))
+        print(join(path, filename))
         # raise Exception
         if exists(join(path, filename)):
             size = getsize(join(path, filename))
@@ -236,10 +264,35 @@ class Downloader(VerbosityMixin):
         :returns:
             (String) The URL to a S3 file
         """
-        if band != 'MTL':
+        if band != 'MTL' and band != 'ANG':
             filename = '%s_B%s.TIF' % (sat['scene'], band)
         else:
             filename = '%s_%s.txt' % (sat['scene'], band)
+        
+        #print (url_builder([self.s3, sat['sat'], sat['path'], sat['row'], sat['scene'], filename]))
+        return url_builder([self.s3, sat['sat'], sat['path'], sat['row'], sat['scene'], filename])
+
+    def amazon_s3_url_type(self, sat, band, type):
+        """
+        Return an amazon s3 url the contains the scene and band provided.
+
+        :param sat:
+            Expects an object created by scene_interpreter method
+        :type sat:
+            dict
+        :type type
+            TIF.ovr of WRK.IMD
+        :param filename:
+            The filename that has to be downloaded from Amazon
+        :type filename:
+            String
+
+        :returns:
+            (String) The URL to a S3 file
+        """
+        file_extentie = '%s_B%s' + type
+        if band != 'MTL' and band != 'ANG':
+            filename = file_extentie % (sat['scene'], band)
 
         return url_builder([self.s3, sat['sat'], sat['path'], sat['row'], sat['scene'], filename])
 
@@ -280,6 +333,12 @@ class Downloader(VerbosityMixin):
             The scene ID.
         :type scene:
             String
+........    Pre-collection data style:
+............LC81970232017085LGN00
+
+........    collection 1 datas style:
+........    LC08_L1TP_139045_20170304_20170316_01_T1
+
 
         :returns:
             dict
@@ -300,10 +359,16 @@ class Downloader(VerbosityMixin):
             'scene': scene
         }
         if isinstance(scene, str) and len(scene) == 21:
+            #LC81970232017085LGN00
             anatomy['path'] = scene[3:6]
             anatomy['row'] = scene[6:9]
             anatomy['sat'] = 'L' + scene[2:3]
-
+            return anatomy
+        elif isinstance(scene, str) and len(scene) == 40:
+            #LC08_L1TP_139045_20170304_20170316_01_T1
+            anatomy['path'] = scene[10:13]
+            anatomy['row'] = scene[13:16]
+            anatomy['sat'] = '/c1/L' + scene[3:4]
             return anatomy
         else:
             raise IncorrectSceneId('Received incorrect scene')
